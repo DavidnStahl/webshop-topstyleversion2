121f2962e744ac81411196088a97d0754406db78
David Ståhl <47720480+DavidnStahl@users.noreply.github.com>
2020-04-14 07:56:58 +0200

    removed google cloud  con string

--- mybackend/server.js
+++ mybackend/server.js
@@ -14,16 +14,16 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
-const con = "Provider=MSOLEDBSQL;Server=35.228.40.196;Database=webshop;UID=sqlserver;PWD=o6iJK5kt4tDk9pmx;Driver={SQL Server Native Client 11.0}";
 
-//const connString = "server=.;Database=webshop;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+
+const connString = "server=.;Database=webshop;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
 
 app.post('/addCustomer', function(req, res){   
     console.log(JSON.parse(req.body))
     let result = JSON.parse(req.body)
     const sqlCode = "INSERT INTO customers (username, password) values('" + result.username + "','" + result.password + "')";
     res.send()
-    sql.query(con, sqlCode, function(err,response){
+    sql.query(connString, sqlCode, function(err,response){
       console.log(err)
      console.log("customer added")
   });
@@ -33,16 +33,16 @@ app.post('/addOrder', function(req, res){
   let result = JSON.parse(req.body)
   let sqlCode = "INSERT INTO orders (totalpayment, customerID) values('" + result.totalpayment + "','" + result.customerID + "')";
   res.send()
-  sql.query(con, sqlCode, function(err,response){
+  sql.query(connString, sqlCode, function(err,response){
    });
    let query = "SELECT TOP 1 (orderID) FROM orders ORDER BY orderID DESC";
-  sql.query(con, query, (err, rows) => {
+  sql.query(connString, query, (err, rows) => {
    let orderID = rows[0].orderID
    result.produktID.forEach(produkt => {
      console.log(produkt)
     sqlCode = "INSERT INTO orders_products (orderID, produktID) values('" + orderID + "','" + produkt + "')";
     res.send()
-    sql.query(con, sqlCode, function(err,response){
+    sql.query(connString, sqlCode, function(err,response){
        });    
      });  
   });  
@@ -50,7 +50,7 @@ app.post('/addOrder', function(req, res){
 app.get('/getProducts', function(request, response){
   let query = "SELECT * FROM products";
 
-  sql.query(con, query, (err, rows) => {
+  sql.query(connString, query, (err, rows) => {
    console.log(rows);
    response.send(rows)
  });
@@ -59,7 +59,7 @@ app.get('/getProduct', function(req, res){
   console.log(req.query)
   let query = `SELECT * FROM products WHERE products.name LIKE '${req.query.name}%'`;
 
-  sql.query(con, query, (err, rows) => {
+  sql.query(connString, query, (err, rows) => {
    console.log(rows);
    res.send(rows)
  });
@@ -67,7 +67,7 @@ app.get('/getProduct', function(req, res){
 app.get('/getCustomers', function(request, response){
    let query = "SELECT * FROM customers";
 
-   sql.query(con, query, (err, rows) => {
+   sql.query(connString, query, (err, rows) => {
     response.send(rows)
   });
 });
